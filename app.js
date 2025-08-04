b30e9f4f4c29eebd46e7e21f485300f8cf1a540e
ualperen41 <alperenb.200021@gmail.com>
2025-08-04 19:03:29 +0300

    finished

--- app.js
+++ app.js
@@ -27,6 +27,20 @@ searchBtn.addEventListener("click",() => {
   search.classList.toggle("active");
 });
 
+searchInput.addEventListener('input', (e) => {
+  //console.log(searchInput.value)
+  const searchValue = searchInput.value.toLowerCase()
+  const pokemonNames = document.querySelectorAll('.poke-name')
+
+  // console.log(pokemonNames)
+
+  pokemonNames.forEach((pokemonName) => {
+     if (!pokemonName.innerHTML.toLowerCase().includes(searchValue)) {
+      pokemonName.parentElement.parentElement.style.display = 'none'
+    }
+  })
+})
+
 const fetchPokemons = async () => {
   for (let i=1; i <= pokemon_count; i++){
     await getPokemon(i)
@@ -42,8 +56,11 @@ const getPokemon = async (id) => {
 }
  const createPokemonCard = (pokemon) => {
   const pokemonDiv = document.createElement("div")
-  pokemonDiv.classList.add("pokemon")
-const pokemonId = pokemon.id.toString().padStart(3, '0')
+  pokemonDiv.classList.add("pokemon");
+const pokemonId = pokemon.id.toString().padStart(3, '0');
+const pokemonType = pokemon.types[0].type.name
+const pokemonBg =bg_color[pokemonType]
+pokemonDiv.style.backgroundColor =`${pokemonBg}`
   const pokemonDivInnerHTML = ` <div class="image-container">
                      <img
               src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
@@ -52,18 +69,18 @@ const pokemonId = pokemon.id.toString().padStart(3, '0')
 
         </div>
         <div class="poke-info">
-          <span class="poke-id">#${pokemon.Id}</span>
+          <span class="poke-id">#${pokemonId}</span>
           <h3 class="poke-name">${pokemon.name}</h3>
           <div class="small">
             <small class="poke-exp">
-              <i class="fa solid fa-flask"></i>100 exp
+              <i class="fa solid fa-flask"></i>${pokemon.base_experience}
             </small>
             <small class="poke-weight"
-              ><i class="fa solid fa-flask"></i>300 kg</small
+              ><i class="fa solid fa-flask"></i>${pokemon.weight}</small
             >
           </div>
           <div class="poke-type">
-            <i class="fa-brands fa-uncharted"></i>grass
+            <i class="fa-brands fa-uncharted"></i>${pokemonType}
           </div>
         </div>`
         pokemonDiv.innerHTML =pokemonDivInnerHTML
